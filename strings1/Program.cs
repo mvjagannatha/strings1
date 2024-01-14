40b364429fc54505b33c1e226fdd2a0d465685dd
Admin <Admin@DESKTOP-6PQE64G>
2024-01-14 18:35:54 +0530

    Add project files.

---
+++ strings1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.Design;
+class program
+{
+    static void Main(string[] args)
+    {
+        string string1 = "jagannatha";
+        string string2 = "mvjagannatha";
+        if(string.Compare(string1, string2) == 0)
+        {
+            Console.WriteLine("strings are equal");
+            
+        }
+        else
+        {
+            Console.WriteLine("strings are not equal");
+        }
+        Console.ReadKey();
+    }
+}
